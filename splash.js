02ecfd82e01de91723b3069f605bf7b53cc8cfc8
Inquerex <98789039+inquerex@users.noreply.github.com>
2022-03-06 17:04:15 -0800

    Add files via upload

--- splash.js
+++ splash.js
@@ -32,3 +32,24 @@ function sendEmail() {
       Body : "Someone downloaded Inquerex.dmg.",
     });
 }
+
+// Get OS. //
+{
+    var OSName = "Unknown OS";
+    if (navigator.userAgent.indexOf("Win") != -1) OSName = "Windows";
+    if (navigator.userAgent.indexOf("Mac") != -1) OSName = "Macintosh";
+    if (navigator.userAgent.indexOf("Linux") != -1) OSName = "Linux";
+    if (navigator.userAgent.indexOf("Android") != -1) OSName = "Android";
+    if (navigator.userAgent.indexOf("like Mac") != -1) OSName = "iOS";
+}
+
+// Redirect if OS isn't Macintosh. //
+function checkOS() {
+    if (OSName !== "Macintosh") {
+        window.open('compatibility.html');
+    } else {
+        window.location.href 
+            = "Inquerex.dmg";
+        sendEmail();
+    }
+}
