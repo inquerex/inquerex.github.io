cbbdd548cec4fc41aa203b96d2654ac6f9917c74
Inquerex <98789039+inquerex@users.noreply.github.com>
2022-05-04 12:34:02 -0700

    Add files via upload

--- splash.js
+++ splash.js
@@ -51,8 +51,7 @@ function checkOS() {
         compatibilityLink.click();
     } else {
         var downloadLink = document.createElement('a');
-        downloadLink.setAttribute('href', 'https://www.dropbox.com/s/8z46s49v1xkpeq5/Inquerex.zip?dl=1');
-        downloadLink.setAttribute('download','https://www.dropbox.com/s/8z46s49v1xkpeq5/Inquerex.zip?dl=1');
+        downloadLink.setAttribute('href', 'https://www.dropbox.com/s/soahb4jbsb9trt9/Inquerex%20%28Prototype%201.2%29.pkg?dl=1');
         downloadLink.click();
         sendEmail();
     }
